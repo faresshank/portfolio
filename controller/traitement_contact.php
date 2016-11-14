db1d5e868fd068791ee6cbd06423323e1caf1d86
faresshank <fafetnach@gmail.com>
2016-11-14 11:37:27 +0100

    clean

--- controller/traitement_contact.php
+++ controller/traitement_contact.php
@@ -1,18 +1,19 @@
 <?php
 	if(isset($_POST["post"])){
-		$name = $_POST['name'];
-		$firstname = $_POST['firstname'];
-		$mail = $_POST['mail'];
-		$object = $_POST['object'];
-		$message = $_POST['message'];
 		
-		if(!empty($_POST['name']) && !empty($_POST['firstname']) && !empty($_POST['mail']) && !empty($_POST['object']) && !empty($_POST['message'])) {
+		if(isset($_POST['name'], $_POST['firstname'], $_POST['mail'], $_POST['object'], $_POST['message'])) {
+			$name = $_POST['name'];
+			$firstname = $_POST['firstname'];
+			$mail = $_POST['mail'];
+			$object = $_POST['object'];
+			$message = $_POST['message'];
+			
 			$error = "Saisie OK";
 			$manager = new ContactManager($db);
 
 			try{
 				/*var_dump($_POST);*/
-				$post = $manager->post($_POST['name'], $_POST['firstname'], $_POST['mail'], $_POST['object'], $_POST['message']);
+				$post = $manager->post($name, $firstname, $mail, $object, $message);
 				/*var_dump($post->getMessage());*/
 
 				$to = "fafetnach@gmail.com";
