83b7130deaa6d6fce577083d3f6b4c7438d2aae1
faresshank <fafetnach@gmail.com>
2016-11-14 10:22:58 +0100

    clean

--- index.php
+++ index.php
@@ -10,8 +10,6 @@ catch (PDOException $e)
 	header("Location: index.php?page=home");
 	exit;
 }
-/*"localhost", "root", "", "portfolio"*/
-/*var_dump($db);*/
 /*var_dump(mysqli_error($db));*/
 function __autoload($className){
 		require('model/'.$className.'.class.php');
@@ -21,7 +19,6 @@ $error = "";
 $error404 = "";
 $page = "home";
 $access = ["home", "portfolio", "contact", "career", "sweethome", "fcostwald"];
-/*$accessAdmin = ["home", "crea_web", "crea_logos", "crea_bonus", "crea_single", "contact", "career", "404"];*/
 
 if(isset($_SESSION["admin"]))
 {
